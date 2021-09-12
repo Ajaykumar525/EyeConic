c8c9f535ea3bed55a89ceaf5b2f0944f0687a8fc
Ajaykumar525 <patelajaykumar525@gmail.com>
2021-09-12 22:35:18 +0930

    Created new project
    Created Web API project. Added product entity and connected with database

---
+++ eyeconic/API/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetProducts()
+        {
+            return "This is a list of eyewear products";
+        }
+
+        [HttpGet("{id}")]
+        public string GetProduct(int id)
+        {
+            return "This would be single product";
+        }
+    }
+}
