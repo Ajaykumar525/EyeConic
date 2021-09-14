7d97811ac43601b4c9cc13bbc94b3bb72951bc35
scpstar <s331393@students.cdu.edu.au>
2021-09-15 00:26:21 +0930

    Created three additional files, added repository pattern, added repo interface and methods, created related entities by extending products entity

--- eyeconic/API/Startup.cs
+++ eyeconic/API/Startup.cs
@@ -1,3 +1,4 @@
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -30,7 +31,7 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddScoped<IProductRepository, ProductRepository>();
             services.AddControllers();
             services.AddDbContextPool<StoreContext>(x => 
                 x.UseSqlServer(_config.GetConnectionString("DefaultConnection")));
