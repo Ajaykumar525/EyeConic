455e99298a92a046b9f04976e90a8cfd21d56fed
nihalmaredia <47981813+nihalmaredia@users.noreply.github.com>
2021-09-15 16:12:16 +0930

    create migration
    Applying the migrations and creating the
    Database at app startup (27)

--- eyeconic/Infrastructure/Data/StoreContext.cs
+++ eyeconic/Infrastructure/Data/StoreContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace Infrastructure.Data
@@ -18,5 +19,11 @@ namespace Infrastructure.Data
         public DbSet<ProductBrand> ProductBrands { get; set; }
 
         public DbSet<ProductType> ProductTypes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
     }
 }
